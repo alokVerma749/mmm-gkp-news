a9eaf23ccd849bbd96d1b4c80c708d6072fd72dc
alokverma <alokverma749@gmail.com>
2025-01-02 00:13:01 +0530

    fix: minor issues

--- middleware.ts
+++ middleware.ts
@@ -7,22 +7,22 @@ const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
 export async function middleware(request: NextRequest) {
   // Restrict access to routes starting with "/admin"
   if (request.nextUrl.pathname.startsWith("/admin")) {
-    // const token = request.cookies.get("auth_token")?.value;
+    const token = request.cookies.get("auth_token")?.value;
 
-    // if (!token) {
-    //   // Redirect to login if no token is found
-    //   return NextResponse.redirect(new URL("/login", request.url));
-    // }
+    if (!token) {
+      // Redirect to login if no token is found
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-    // try {
-    //   // Verify the token
-    //   const secret = new TextEncoder().encode(JWT_SECRET);
-    //   await jwtVerify(token, secret);
-    // } catch (error) {
-    //   console.error("Invalid Token:", error);
-    //   // Redirect to login if token is invalid
-    //   return NextResponse.redirect(new URL("/login", request.url));
-    // }
+    try {
+      // Verify the token
+      const secret = new TextEncoder().encode(JWT_SECRET);
+      await jwtVerify(token, secret);
+    } catch (error) {
+      console.error("Invalid Token:", error);
+      // Redirect to login if token is invalid
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 
   // Allow the request to proceed for non-restricted routes
