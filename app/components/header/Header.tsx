c8465fdf57323ebf8a9f1a70cc89bb54a92aaa61
alokverma <alokverma749@gmail.com>
2025-01-02 19:46:25 +0530

    fix: rerenders due to weather api

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -104,7 +104,7 @@ const Header: React.FC = () => {
               </li>
             );
           })}
-          <Weather />
+          {/* <Weather /> */}
         </ul>
       </div>
     </div>
