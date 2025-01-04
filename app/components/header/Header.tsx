63786a3e2d6f9a344f2dd776c092b113c9d78c47
alokverma <alokverma749@gmail.com>
2025-01-04 20:23:46 +0530

    fix: minor ui

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -26,14 +26,11 @@ function useDebounce<T>(value: T, delay: number): T {
 
 const Header: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [suggestions, setSuggestions] = useState<
-    { _id: string; title: string }[]
-  >([]);
-  const [currentTemperature, setCurrentTemperature] = useState<number | null>(
-    null
-  );
+  const [suggestions, setSuggestions] = useState<{ _id: string; title: string }[]>([]);
+  const [currentTemperature, setCurrentTemperature] = useState<number | null>(null);
   const [weatherLoading, setWeatherLoading] = useState<boolean>(true);
   const [weatherError, setWeatherError] = useState<string | null>(null);
+  const [sidebar, setSidebar] = useState(false);
 
   const tags: string[] = [
     "Department",
@@ -101,7 +98,6 @@ const Header: React.FC = () => {
     setSearchTerm(e.target.value);
   };
 
-  const [sidebar, setSidebar] = useState(false);
   const handleSidebarToggle = () => {
     setSidebar(!sidebar);
   };
@@ -151,10 +147,7 @@ const Header: React.FC = () => {
               <li key={tag} className="h-0 lg:h-auto p-2">
                 <Link
                   href={tagPath}
-                  className={`${isActive
-                      ? "bg-[#04594D] h-full p-2 font-extralight"
-                      : "text-white"
-                    }`}
+                  className={`${isActive ? "bg-[#04594D] h-full p-2 font-extralight" : "text-white"}`}
                 >
                   {tag}
                 </Link>
@@ -174,6 +167,62 @@ const Header: React.FC = () => {
           )}
         </ul>
       </div>
+
+      {/* Sidebar for mobile device */}
+      <div
+        className="text-white text-center lg:hidden flex items-center justify-center h-auto pb-2"
+        onClick={handleSidebarToggle}
+      >
+        {sidebar ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            className="h-4"
+          >
+            <path
+              fill="#ffffff"
+              d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className="h-4"
+          >
+            <path
+              fill="#fbfefe"
+              d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+            />
+          </svg>
+        )}
+      </div>
+
+      {/*Sidebar for mobile device*/}
+      {sidebar && (
+        <div
+          className={`lg:hidden flex flex-col justify-center items-start pl-4 bg-[#1f1f1f] text-white transform transition-all duration-[0.9s] 
+        ${sidebar ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}
+          style={{
+            height: sidebar ? "auto" : "0",
+            opacity: sidebar ? "1" : "0",
+          }}
+        >
+          <ul className="text-sm font-thin w-full">
+            {tags.map((tag) => {
+              const tagPath = `/${tag.toLowerCase().replace(" ", "_")}`;
+              const isActive = activePath === tagPath;
+              return (
+                <li key={tag} className="p-2 w-full">
+                  <Link href={tagPath} className={`${isActive ? "bg-[#04594D] w-full p-2 block" : "p-2 text-white"}`}>
+                    {tag}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
