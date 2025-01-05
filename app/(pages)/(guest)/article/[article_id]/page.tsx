b3a55907d709d2c33f258cbc436004482048c9a0
Shubham <sk715152@gmail.com>
2025-01-05 12:09:35 +0530

    article-page

--- app/(pages)/(guest)/article/[article_id]/page.tsx
+++ app/(pages)/(guest)/article/[article_id]/page.tsx
@@ -1,9 +1,9 @@
-'use server'
+"use server";
 
 import Image from "next/image";
 import { Metadata } from "next";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
-import Markdown from 'react-markdown';
+import Markdown from "react-markdown";
 import getArticleAction from "@/app/Actions/get-article/getArticle";
 import { Article as ArticleType } from "@/app/types/article";
 import downvoteArticleAction from "@/app/Actions/downvote-article/downvoteArticle";
@@ -13,7 +13,9 @@ type ArticleProps = {
   params: Promise<{ article_id: string }>; // Note: Keeping this async if required
 };
 
-export async function generateMetadata({ params }: ArticleProps): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: ArticleProps): Promise<Metadata> {
   const { article_id } = await params;
 
   const response: string = await getArticleAction(article_id);
@@ -38,7 +40,8 @@ export default async function Article({ params }: ArticleProps) {
   const response: string = await getArticleAction(article_id);
   const article: ArticleType | null = response ? JSON.parse(response) : null;
 
-  const defaultImage = "https://res.cloudinary.com/dv1fxqrsi/image/upload/v1735705725/article-thumbnails/b4lxfvb1qgkefdbybuor.jpg";
+  const defaultImage =
+    "https://res.cloudinary.com/dv1fxqrsi/image/upload/v1735705725/article-thumbnails/b4lxfvb1qgkefdbybuor.jpg";
   const imageSrc = article?.image || defaultImage;
 
   if (!article) {
@@ -46,27 +49,27 @@ export default async function Article({ params }: ArticleProps) {
   }
 
   async function onUpvote(data: FormData) {
-    'use server'
+    "use server";
 
-    await upvoteArticleAction(data)
+    await upvoteArticleAction(data);
   }
 
   async function onDownvote(data: FormData) {
-    'use server'
+    "use server";
 
-    await downvoteArticleAction(data)
+    await downvoteArticleAction(data);
   }
 
   return (
     <div className="">
       <div className="w-full mx-auto relative">
-        <div className="w-full h-[60%] bg-foreground absolute top-0 z-0"></div>
+        <div className="w-full h-[60%] bg-[#1A1A1A] lg:bg-foreground absolute top-0 z-0"></div>
         {/* Render static initial articles */}
-        <div className="relative z-10 pt-1">
-          <div className="w-3/4 mx-auto border overflow-hidden border-gray-800 rounded-xl shadow-lg shadow-slate-700 custom-height-1 custom-height-2 custom-height-3">
+        <div className="relative z-10 px-2 lg:px-0">
+          <div className="lg:w-3/4 lg:mx-auto border rounded-lg overflow-hidden border-gray-800 lg:rounded-none lg:rounded-b-xl shadow-lg shadow-slate-700 custom-height-1 custom-height-2 custom-height-3">
             <Image
               alt={`${article.title} thumbnail`}
-              className="flex-shrink-0 rounded-b-xl w-full h-96 object-cover object-center"
+              className="flex-shrink-0 w-full h-96 object-cover object-center"
               src={imageSrc}
               width={200}
               height={200}
@@ -74,38 +77,49 @@ export default async function Article({ params }: ArticleProps) {
           </div>
         </div>
       </div>
-
-      <div className="flex gap-4 w-3/4 mx-auto mt-16">
-        {/* Upvote Form */}
-        <form action={onUpvote} className="flex justify-center items-center">
-          <input type="hidden" name="article_id" value={article_id} />
-          <button type="submit" className="flex justify-center items-center bg-[#04594D] text-white px-4 py-1 rounded-md animate-bounce">
-            <ArrowBigUp strokeWidth={1.75} />
-            <p className="text-xs">Upvote</p>
-          </button>
-        </form>
-        {/* Downvote Form */}
-        <form action={onDownvote} className="flex justify-center items-center">
-          <input type="hidden" name="article_id" value={article_id} />
-          <button type="submit" className="flex justify-center items-center bg-gray-700 text-white px-4 py-1 rounded-md">
-            <ArrowBigDown strokeWidth={1.75} />
-            <p className="text-xs">Downvote</p>
-          </button>
-        </form>
-      </div>
-
-      <div className="w-3/4 mx-auto mt-4">
-        <h1 className="text-xl font-semibold my-2 mb-4">{article.title}</h1>
-
-        <div className="markdown-content">
-          <Markdown>{article.content}</Markdown>
+      <div className="w-[80%] lg:w-3/4 mx-auto">
+        <div className="flex gap-4 mt-12 lg:mt-16">
+          {/* Upvote Form */}
+          <form action={onUpvote} className="flex justify-center items-center">
+            <input type="hidden" name="article_id" value={article_id} />
+            <button
+              type="submit"
+              className="flex justify-center items-center bg-[#04594D] text-white px-4 py-1 rounded-md animate-bounce"
+            >
+              <ArrowBigUp strokeWidth={1.75} />
+              <p className="text-xs">Upvote</p>
+            </button>
+          </form>
+          {/* Downvote Form */}
+          <form
+            action={onDownvote}
+            className="flex justify-center items-center"
+          >
+            <input type="hidden" name="article_id" value={article_id} />
+            <button
+              type="submit"
+              className="flex justify-center items-center bg-gray-700 text-white px-4 py-1 rounded-md"
+            >
+              <ArrowBigDown strokeWidth={1.75} />
+              <p className="text-xs">Downvote</p>
+            </button>
+          </form>
+          
         </div>
 
-        <div className="mt-16">
-          <div className="flex justify-center items-center">
-            <p className="text-sm">Total</p>
-            <ArrowBigUp strokeWidth={1} />
-            <p className="text-sm">{article.upvotes}</p>
+        <div className=" mt-4">
+          <h1 className="text-xl font-semibold my-2 mb-4">{article.title}</h1>
+          <div className="h-[1.8] w-[80%] bg-[#c7c7c7] my-2 lg:my-4"></div>
+          <div className="markdown-content">
+            <Markdown>{article.content}</Markdown>
+          </div>
+
+          <div className="mt-16">
+            <div className="flex justify-center items-center">
+              <p className="text-sm">Total</p>
+              <ArrowBigUp strokeWidth={1} />
+              <p className="text-sm">{article.upvotes}</p>
+            </div>
           </div>
         </div>
       </div>
