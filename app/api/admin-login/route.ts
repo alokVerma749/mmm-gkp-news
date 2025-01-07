9f82c946e727175115889a11e306bf755803c214
alokverma <alokverma749@gmail.com>
2025-01-07 15:07:05 +0530

    fix: build

--- app/api/admin-login/route.ts
+++ app/api/admin-login/route.ts
@@ -31,12 +31,12 @@ export async function POST(request: Request) {
     }
 
     // OTP verification
-    // if (!otp || admin.otp != otp) {
-    //   return new Response(
-    //     JSON.stringify({ message: "Invalid credentials" }),
-    //     { status: 401, headers: { "Content-Type": "application/json" } }
-    //   );
-    // }
+    if (!otp || admin.otp != otp) {
+      return new Response(
+        JSON.stringify({ message: "Invalid credentials" }),
+        { status: 401, headers: { "Content-Type": "application/json" } }
+      );
+    }
 
     // Generate JWT token
 
