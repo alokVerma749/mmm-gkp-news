f55020830ca378a8ef011b78a673a4972477170f
alokverma <alokverma749@gmail.com>
2025-03-13 01:40:43 +0530

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // Add Cloudinary's hostname here
-  }
+    domains: ["res.cloudinary.com"],
+  },
+  webpack: (config:any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "@langchain/google-genai", "@langchain/community/vectorstores/chroma"];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
